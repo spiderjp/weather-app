589321fc866d5b0824492ec952bbc8cbd23ff0fb
spiderjp <jotapefsbherofc@gmail.com>
2022-07-28 14:08:46 -0300

    Finalizado com sucesso

---
+++ script.js
@@ -0,0 +1,54 @@
+let weather = {
+    "apiKey" : "{colocar a chave da api aqui}",
+
+    fetchWeather: function(city){
+
+        /* fetch é o método global, 
+        e a forma comum de buscar informações assíncronas por meio do JS, 
+        quando usamos uma API Fetch
+        */
+       
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + city  
+        + "&units=metric&appid=" 
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "ºC";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+
+document.querySelector(".search button").addEventListener("click", function(){
+
+    weather.search();
+
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if (event.key == "Enter"){
+        weather.search();
+    }
+});
+
+
+weather.fetchWeather("São Paulo");
+
